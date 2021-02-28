2f9c6fd26d80eb6e70832ff65bef76827192cac8
alexzhuravlov13 <alexzhuravlov13@gmail.com>
2021-02-28 18:44:16 +0200

    fix userservice test

--- src/test/java/com/zhuravlov/service/UserServiceTest.java
+++ src/test/java/com/zhuravlov/service/UserServiceTest.java
@@ -1,16 +1,11 @@
 package com.zhuravlov.service;
 
-import com.zaxxer.hikari.HikariDataSource;
 import com.zhuravlov.db.Dao.UserDaoImpl;
 import com.zhuravlov.model.builder.UserEntityBuilder;
 import com.zhuravlov.model.entity.UserEntity;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,10 +17,6 @@ import static org.mockito.Mockito.when;
 public class UserServiceTest {
     private UserService service;
     private UserDaoImpl mockDao;
-    private Connection mockConn;
-    private HikariDataSource mockDatasource;
-    private PreparedStatement mockPs;
-    private ResultSet mockResultSet;
 
 
     @Before
@@ -38,7 +29,7 @@ public class UserServiceTest {
     public void create() {
         UserEntity userEntity = UserEntityBuilder.getInstance().build();
         when(mockDao.create(any(UserEntity.class))).thenReturn(userEntity);
-
+        assertEquals(userEntity, service.create(userEntity));
     }
 
     @Test
@@ -90,7 +81,7 @@ public class UserServiceTest {
     @Test
     public void registerUser() {
         UserEntity userEntity = UserEntityBuilder.getInstance().setFirstName("").setLastName("").setPassword("1").setEmail("2")
-        .build();
+                .build();
         when(mockDao.create(any(UserEntity.class))).thenReturn(userEntity);
         assertEquals(userEntity, service.registerUser("", "", "1", "2"));
 
